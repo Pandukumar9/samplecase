a71db8f51ec1a20b5c83dd508f14e25ba92ee212
pandukumar9 <pandukumar.gujja@gmail.com>
2021-07-21 13:22:21 +0530

    student component add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StudentAddComponent } from './components/student-add/student-add.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'student',component:StudentAddComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentAddComponent } from './components/student-add/student-add.component';
+import { StudentListComponent } from './components/student-list/student-list.component';
+import { StudentDetailsComponent } from './components/student-details/student-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentAddComponent,
+    StudentListComponent,
+    StudentDetailsComponent
   ],
   imports: [
     BrowserModule,
